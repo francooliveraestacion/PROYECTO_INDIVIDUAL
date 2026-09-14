d37a9fdd2b1985a66f6e1379869272b1ee067083
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 17:32:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Indicadores De Despacho")
 st.sidebar.title("Contenido")
 st.write("Elaborado:Franco Olivera")
-st.sidebar.selectbox("Home:","Ejerccio 1","Ejerccio 2",
-                     "Ejerccio 3","Ejerccio 4")
+st.sidebar.selectbox("",["Home","Ejerccio 1","Ejerccio 2",
+                     "Ejerccio 3","Ejerccio 4"])
