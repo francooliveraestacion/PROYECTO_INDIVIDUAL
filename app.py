046c6fc6bed6b364f3eab6bcf0ae0adbadbad3d9
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 18:07:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ contenido=st.sidebar.selectbox("",["Home","Ejerccio 1","Ejerccio 2",
 
 if contenido =="Home":
   st.write("Te encuentras em el modulo de home")
+  st.subheaher("Estudiante")
 
 
 elif contenido ==("Ejerccio 1"):
