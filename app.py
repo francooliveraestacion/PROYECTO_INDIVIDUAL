a676b839f6f3b443c5cfb44a43272213971ada88
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 18:00:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,25 @@ import streamlit as st
 st.title("Indicadores De Despacho")
 st.sidebar.title("Contenido")
 st.write("Elaborado:Franco Olivera")
-st.sidebar.selectbox("",["Home","Ejerccio 1","Ejerccio 2",
+contenido=st.sidebar.selectbox("",["Home","Ejerccio 1","Ejerccio 2",
                      "Ejerccio 3","Ejerccio 4"])
+
+if contenido =="Home":
+  st.write("Te encuentras em el modulo de home")
+
+
+elif contenido ==("Ejerccio 1"):
+  st.write("Te encuentras em el modulo de Ejerccio 1")
+
+elif contenido ==("Ejerccio 2"):
+  st.write("Te encuentras em el modulo de Ejerccio 2")
+
+elif contenido ==("Ejerccio 3"):
+  st.write("Te encuentras em el modulo de Ejerccio 3")
+
+else  contenido ==("Ejerccio 4 "):
+  st.write("Te encuentras em el modulo de Ejerccio 4")
+
+
+
+
