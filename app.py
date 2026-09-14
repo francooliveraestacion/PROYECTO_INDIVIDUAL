312b451fe0a08a92d608853c07b7b1625827c455
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 18:13:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ contenido=st.sidebar.selectbox("",["Home","Ejerccio 1","Ejerccio 2",
                      "Ejerccio 3","Ejerccio 4"])
 
 if contenido =="Home":
-  st.write("Te encuentras em el modulo de home")
+  st.write("Te encuentras en el modulo de home")
   st.subheader("Estudiante")
   st.write ("Franco Olivera Esatcion")
 
 
 elif contenido ==("Ejerccio 1"):
-  st.write("Te encuentras em el modulo de Ejerccio 1")
+  st.write("Te encuentras en el modulo de Ejerccio 1")
 
 elif contenido ==("Ejerccio 2"):
-  st.write("Te encuentras em el modulo de Ejerccio 2")
+  st.write("Te encuentras en el modulo de Ejerccio 2")
 
 elif contenido ==("Ejerccio 3"):
-  st.write("Te encuentras em el modulo de Ejerccio 3")
+  st.write("Te encuentras en el modulo de Ejerccio 3")
 
 else:
-  st.write("Te encuentras em el modulo de Ejerccio 4")
+  st.write("Te encuentras en el modulo de Ejerccio 4")
 
 
 
