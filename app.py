401f94c6e1aaa0595583e26e69211a84a5ffd737
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 18:35:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ de información y el seguimiento de los principales indicadores.
 """)
  st.subheader("Tecnologias utilizadas")
  st.markdown("""
-- 🐍 Python
-- 📊 Streamlit
-- 📈 Pandas
-- 📉 Matplotlib
+ 🐍 Python
+ 📊 Streamlit
+ 📈 Pandas
+ 📉 Matplotlib
 """)
   
   
