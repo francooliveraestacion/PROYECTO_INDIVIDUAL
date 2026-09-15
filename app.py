b1a3a133d7ab1f9231b801b4a371046d3a5bc51f
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 20:56:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -59,10 +59,12 @@ el total de gastos y el saldo final.
 
     st.subheader("Movimientos registrados")
     if len(st.session_state.movimientos) > 0:
-     st.dataframe(df)
-      total_ingreso = df.loc[df["tipo"] == "ingreso","valor"].sum()
-      total_gasto = df.loc[df["tipo"] == "gasto","valor"].sum()
-      saldo_final = total_ingreso - total_gasto
+    st.dataframe(df)
+
+    total_ingreso = df.loc[df["tipo"] == "ingreso", "valor"].sum()
+    total_gasto = df.loc[df["tipo"] == "gasto", "valor"].sum()
+    saldo_final = total_ingreso - total_gasto
+   
     st.subheader("Resultado del flujo de caja")
     col1, col2, col3 = st.columns(3)
     with col1:
