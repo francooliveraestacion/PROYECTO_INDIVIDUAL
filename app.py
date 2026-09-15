14f21db4a3d5abb4add90aa1499cb84badac81c7
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 19:40:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,14 @@ el total de gastos y el saldo final.
     tipo=st.selectbox("Tipo de movimiento:",
                       ["Ingreso","Gasto"])
     valor=st.text_input("Valor,min_value==0.0,step=0.01")
+    if st.botton("Agregar un boton"):
+      if concepto == "":
+        st.warning("Ingrese un concepto")
+
+      elif valor <=0 :
+        st.warning("Ingrese un calor mayor que 0.")
+      else:
+        movimiento={"concepto":concepto,"tipo":tipo,"valor":valot}
     
     
     
