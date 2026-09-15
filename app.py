52fb2b31f095a2874723e9c62248780fdb1c7a07
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 21:06:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ el total de gastos y el saldo final.
     st.subheader("Movimientos registrados")
     if len(st.session_state.movimientos) > 0:
      st.dataframe(df)
-      total_ingresos = df.loc[df["tipo"] == "ingreso", "valor"].sum()
+      total_ingresos = df.loc[df["tipo"] =="ingreso","valor"].sum()
       
      
 
