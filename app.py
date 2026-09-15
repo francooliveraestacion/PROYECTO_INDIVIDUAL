2da2ef9a799f217066a338792c255cc34382adaa
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 21:02:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,14 +60,7 @@ el total de gastos y el saldo final.
     st.subheader("Movimientos registrados")
     if len(st.session_state.movimientos) > 0:
      st.dataframe(df)
-      total_ingreso = df.loc[df["tipo"] == "ingreso","valor"].sum()
-
-      total_gasto = df.loc[df["tipo"] == "gasto","valor"].sum()
-
-    st.subheader("Resultado del flujo de caja")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.metric("Total de ingresos", f"S/ {total_ingreso:.2f}")
+     
 
     
     
