b04100b3f6949d88384c0c35c45ab1631b5183e0
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-14 20:39:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ el total de gastos y el saldo final.
       saldo_final = total_ingresos - total_gastos
     st.subheader("Resultado del flujo de caja")
     col1, col2, col3 = st.columns(3)
-     with col1:
+    with col1:
         st.metric("Total de ingresos", f"S/ {total_ingresos:.2f}")
 
     with col2:
@@ -74,7 +74,7 @@ el total de gastos y el saldo final.
     with col3:
         st.metric("Saldo final", f"S/ {saldo_final:.2f}")
 
-    # Indicar situación del flujo
+   
     if saldo_final >= 0:
         st.success("El flujo de caja está a favor.")
     else:
