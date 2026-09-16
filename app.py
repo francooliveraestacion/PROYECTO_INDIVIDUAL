08d9de46047b48aab7f2d952ce4090a66bc5c9e0
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-15 19:24:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -128,20 +128,8 @@ if len(st.session_state.movimientos) > 0:
         st.success("El flujo de caja está a favor.")
     else:
         st.error("El flujo de caja está en contra.")
-
-else:
+  else:
     st.info("Aún no hay movimientos registrados.")
-  
-  
-
-
-       
-
-      
-     
-
-    
-    
    
           
  elif contenido ==("Ejerccio 2"):
