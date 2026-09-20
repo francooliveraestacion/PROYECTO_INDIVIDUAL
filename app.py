33989baa0bd672041e84f4982cc0703feddc937c
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-19 22:45:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,6 @@ elif contenido ==("Ejerccio 3"):
   st.title("Uso de funciones desde una librería externa")
   if "historico" not in st.session_state:st.session_state.historico = []
   funcion = st.selectbox("Seleccione una función",["Calcular IMC"])
-  nombre= st.imput("Ingrese su nombre:")
   peso_kg= st.number_input( "Ingrese el peso (kg)",min_value=0.1,value=70.0)
   altura_m = st.number_input("Ingrese la altura (m)", min_value=0.1, value=1.70)
   
