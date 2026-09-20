c9a9103123dfaab189381df0c903a34027caea7f
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-19 23:11:51 -0500

    Update librería_clases_proyecto1.py

--- librería_clases_proyecto1.py
+++ librería_clases_proyecto1.py
@@ -330,7 +330,7 @@ class Paciente:
     como IMC y superficie corporal.
     """
 
-    def __init__(self, nombre, peso_kg, altura_m):
+    def imc_corporal(self, nombre, peso_kg, altura_m):
         self.nombre = nombre
         self.peso_kg = peso_kg
         self.altura_m = altura_m
