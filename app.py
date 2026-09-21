ecdeaf43265a95f7ed7cbbca04671628f0b02512
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-20 19:32:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -208,13 +208,7 @@ else:
         key="crear_stock"
     )
 
-    stock_minimo = st.number_input(
-        "Stock mínimo",
-        min_value=0,
-        value=0,
-        step=1,
-        key="crear_stock_minimo"
-    )
+   
 
   
  
